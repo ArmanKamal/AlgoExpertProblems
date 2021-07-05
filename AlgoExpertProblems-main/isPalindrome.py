da76a242870464f2f5e2ecca7d73fb2fe2b255d3
ArmanKamal <armankamal2421@gmail.com>
2021-07-05 09:47:27 -0500

    Deleted the sub folder

--- AlgoExpertProblems-main/isPalindrome.py
+++
@@ -1,7 +0,0 @@
-def isPalindrome(string):
-    # Write your code here.
-	reversedString = ""
-	for i in reversed(range(len(string))):
-		reversedString += string[i]
-	return string == reversedString
-
